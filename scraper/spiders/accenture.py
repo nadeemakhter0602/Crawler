26ef1a5d30bd23f02a461233901f9f5d823c6e23
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-01-20 13:36:58 +0530

    follow links from sitemap and get job posting info

--- scraper/spiders/accenture.py
+++ scraper/spiders/accenture.py
@@ -7,9 +7,21 @@ class AccentureSpider(scrapy.Spider):
     allowed_domains = ['accenture.com']
     start_urls = ['https://www.accenture.com/in-en/jobpostings-sitemap.xml']
 
-
     def parse(self, response):
         # inbuilt response.xpath() not working for some reason
         sel = Selector(text=response.text)
-        job_posting_links = sel.xpath('//loc/text()').getall()
-        pass
+        for job_posting_link in sel.xpath('//loc/text()').getall():
+            # follow link and get job posting data
+            request = scrapy.Request(job_posting_link,
+                                     callback=self.parse_job_posting)
+            yield request
+
+    def parse_job_posting(self, response):
+        # job dictionary
+        job_posting = {
+            # get job title
+            'job_title': response.xpath('//h1/text()').get(),
+            # get job location city
+            'job_city': response.xpath('//div/span/text()').get()
+        }
+        yield job_posting
